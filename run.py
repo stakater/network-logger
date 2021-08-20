86e2088aaf345058e5188c052f8d71e48d4aeacd
Tanveer Alam <tan24alam11@gmail.com>
2021-08-20 11:58:25 +0530

    added htts support

--- run.py
+++ run.py
@@ -2,4 +2,9 @@ import os
 from app.nl_app import nlapp
 
 port = os.environ.get("APP_PORT")
-nlapp.run(host='0.0.0.0', debug=False, port=port)
+https = os.environ.get("HTTPS")
+print(port, https)
+if https:
+    nlapp.run(host='0.0.0.0', debug=True, port=port, ssl_context='adhoc')
+else:
+    nlapp.run(host='0.0.0.0', debug=True, port=port)
